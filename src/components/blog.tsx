ccca5e66b98066a218034cb1d8e976a9fddcd43f
ee panyakorn <panyakorn40@gmail.com>
2023-12-16 11:53:38 +0700

    📷 feat: add blog images and service descriptions
    📷 feat: add blog images blog-1.jpg, blog-2.jpg, and blog-3.jpg to the public/img directory
    
    📄 feat: add our-service.json file with service descriptions in the src/assets/json directory
    
    🐛 fix(blog.tsx): rename `blogPosts` variable to `posts` for better naming consistency
    ✨ feat(blog.tsx): update blog component to use Next.js `Image` component for optimized image loading and add category and author information to each blog post

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -1,54 +1,59 @@
 import React from 'react'
-
+import Image from 'next/image'
 type Props = {}
-
-const blogPosts = [
+const posts = [
     {
         id: 1,
-        title: 'Vel expedita assumenda placeat aut nisi optio voluptates quas',
+        title: 'Boost your conversion rate',
         href: '#',
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+        imageUrl: "/img/blog-1.jpg",
         date: 'Mar 16, 2020',
         datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '#' },
         author: {
             name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
             imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                '/img/blog-1.jpg',
         },
     },
     {
-        id: 1,
-        title: 'Vel expedita assumenda placeat aut nisi optio voluptates quas',
+        id: 2,
+        title: 'Boost your conversion rate',
         href: '#',
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-        date: 'Mar 16, 2020',
+        imageUrl: "/img/blog-2.jpg",
+            date: 'Mar 16, 2020',
         datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '#' },
         author: {
             name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
             imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                '/img/blog-1.jpg',
         },
     },
     {
-        id: 1,
-        title: 'Vel expedita assumenda placeat aut nisi optio voluptates quas',
+        id: 3,
+        title: 'Boost your conversion rate',
         href: '#',
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+        imageUrl: "/img/blog-3.jpg",
         date: 'Mar 16, 2020',
         datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '#' },
         author: {
             name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
             imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                '/img/blog-1.jpg',
         },
     },
     // More posts...
@@ -56,45 +61,53 @@ const blogPosts = [
 
 const Blog = (props: Props) => {
     return (
-        <div className=" bg-white py-10 px-6  lg:px-8">
-            <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-                <p className="mt-2 text-lg leading-8 text-gray-600">
-                    Vel dolorem qui facilis soluta sint aspernatur totam cumque.
-                </p>
-            </div>
-            <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                {blogPosts.map((post) => (
-                    <article
-                        key={post.id}
-                        className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
-                    >
-                        <img src={post.imageUrl} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
-                        <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-                        <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-
-                        <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-                            <time dateTime={post.datetime} className="mr-8">
-                                {post.date}
-                            </time>
-                            <div className="-ml-4 flex items-center gap-x-4">
-                                <svg viewBox="0 0 2 2" className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
-                                    <circle cx={1} cy={1} r={1} />
-                                </svg>
-                                <div className="flex gap-x-2.5">
-                                    <img src={post.author.imageUrl} alt="" className="h-6 w-6 flex-none rounded-full bg-white/10" />
-                                    {post.author.name}
+        <div className="bg-white py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl text-center">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+                    <p className="mt-2 text-lg leading-8 text-gray-600">
+                        Learn how to grow your business with our expert advice.
+                    </p>
+                </div>
+                <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    {posts.map((post) => (
+                        <article key={post.id} className="flex flex-col items-start justify-between border-2 border-primary-600 p-4 rounded-lg">
+                            <div className="relative w-full">
+                                <Image
+                                    src={post.imageUrl}
+                                    alt=""
+                                    className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                                    layout="responsive"
+                                    width={500}
+                                    height={500}
+                                />
+                                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                            </div>
+                            <div className="max-w-xl">
+                                <div className="mt-8 flex items-center gap-x-4 text-xs">
+                                    <time dateTime={post.datetime} className="text-gray-500">
+                                        {post.date}
+                                    </time>
+                                    <a
+                                        href={post.category.href}
+                                        className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                                    >
+                                        {post.category.title}
+                                    </a>
+                                </div>
+                                <div className="group relative">
+                                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                        <a href={post.href}>
+                                            <span className="absolute inset-0" />
+                                            {post.title}
+                                        </a>
+                                    </h3>
+                                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
                                 </div>
                             </div>
-                        </div>
-                        <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
-                            <a href={post.href}>
-                                <span className="absolute inset-0" />
-                                {post.title}
-                            </a>
-                        </h3>
-                    </article>
-                ))}
+                        </article>
+                    ))}
+                </div>
             </div>
         </div>
     )
